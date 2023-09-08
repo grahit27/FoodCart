d780645aaf7b5aa1c86df443c6e6b96e6bf9e844
grahit27 <grahit27@gmail.com>
2023-09-09 00:18:25 +0530

    Included Class Based Components

--- src/components/About.js
+++ src/components/About.js
@@ -1,40 +1,47 @@
 import React from "react";
 import { state, setState } from "react";
 import Profile from "./Profile";
-export const About = () => {
-  return (
-    <>
-      <div className="about-page">
-        <h1>About Us Page</h1>
-        <p>Some info about the restaurant</p>
-      </div>
-    </>
-  );
-};
+// export const About = () => {
+//   return (
+//     <>
+//       <div className="about-page">
+//         <h1>About Us Page</h1>
+//         <p>Some info about the restaurant</p>
+//       </div>
+//     </>
+//   );
+// };
 
-export class About2 extends React.Component {
+class About2 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 1,
+      timerSwitch: false,
     };
   }
-  componentDidMount() {
-    this.timer = setInterval(() => {
-      console.log("Timer 1");
-    }, "1000");
-  }
+  componentDidMount() {}
   componentDidUpdate(prevProps, prevState) {
     if (this.state.count !== prevState.count) {
       console.log("State changed");
-      clearInterval(this.timer);
+    }
+    if (this.state.timerSwitch !== prevState.timerSwitch) {
+      if (this.state.timerSwitch) {
+        // Create a new Timer
+        this.timer = setInterval(() => {
+          console.log("Timer 1");
+        }, "1000");
+      }
+      if (!this.state.timerSwitch) {
+        clearInterval(this.timer);
+      }
     }
   }
   componentWillUnmount() {
     clearInterval(this.timer);
   }
   render() {
-    const { count } = this.state;
+    const { count, timerSwitch } = this.state;
     return (
       <>
         <div className="p-5">
@@ -47,12 +54,38 @@ export class About2 extends React.Component {
               });
             }}
           >
-            Increase Count And Stop Timer
+            Increase Count
           </button>
           <h1 className="text-xl font-semibold">Final Value of {count}</h1>
+
+          {/* Timer Button */}
+          {timerSwitch ? (
+            <button
+              className="bg-red-400 rounded-xl m-2 p-2"
+              onClick={() => {
+                this.setState({
+                  timerSwitch: false,
+                });
+              }}
+            >
+              Switch Off Timer
+            </button>
+          ) : (
+            <button
+              className="bg-green-400 rounded-xl m-2 p-2"
+              onClick={() => {
+                this.setState({
+                  timerSwitch: true,
+                });
+              }}
+            >
+              Switch On Timer
+            </button>
+          )}
           <Profile name="Tester Name" />
         </div>
       </>
     );
   }
 }
+export default About2;
