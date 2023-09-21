c55a52b360dd4cbf785636794c09ebc38579c3d0
grahit27 <grahit27@gmail.com>
2023-09-21 23:02:38 +0530

    Fixed Hide Button of Accordian

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,78 @@
 import React from "react";
 import { state, setState } from "react";
+import { useState } from "react";
 import Profile from "./Profile";
-// export const About = () => {
-//   return (
-//     <>
-//       <div className="about-page">
-//         <h1>About Us Page</h1>
-//         <p>Some info about the restaurant</p>
-//       </div>
-//     </>
-//   );
-// };
+import { sampleData } from "../utils/mockData";
+const Section = ({
+  sectionHeading,
+  sectionText,
+  visibility,
+  setVisibility,
+}) => {
+  // const [visibility, setVisibility] = useState(false); //First build logic with State Variables then replace it with parent state vars
+  return (
+    <div className="p-2 m-2 border-2 rounded-md border-purple-400  ">
+      <h1 className="font-semibold text-xl">{sectionHeading}</h1>
+      {visibility ? (
+        <>
+          <button
+            className="p-1 underline-offset-1 rounded-md bg-purple-700 text-white"
+            onClick={() => {
+              setVisibility(null); //Remove Everything from section config
+            }}
+          >
+            Hide
+          </button>
+          <p>{sectionText}</p>
+        </>
+      ) : (
+        <button
+          className="p-1 underline-offset-1 rounded-md bg-purple-700 text-white"
+          onClick={() => {
+            setVisibility(sectionHeading); //Change the section config
+          }}
+        >
+          Show
+        </button>
+      )}
+    </div>
+  );
+};
+export const About = () => {
+  const [sectionConfig, setSectionConfig] = useState("Section-1"); //Mistake Exists in clearing of the accordian
+  const setVisibilityConfig = (section) => setSectionConfig(section);
+  return (
+    <>
+      <div className="about-page">
+        <h1 className="p-2 bg-orange-400 text-center text-3xl">
+          About Us Page
+        </h1>
+        <Section
+          sectionHeading="Section-1"
+          sectionText={sampleData}
+          visibility={sectionConfig === "Section-1"}
+          setVisibility={setVisibilityConfig} // We are sending a function to set the state
+        />
+        <Section
+          sectionHeading="Section-2"
+          sectionText={sampleData}
+          visibility={sectionConfig === "Section-2"}
+          // setIsVisible={() => setSectionConfig("Section-2")}
+          setVisibility={setVisibilityConfig}
+        />
+        <Section
+          sectionHeading="Section-3"
+          sectionText={sampleData}
+          visibility={sectionConfig === "Section-3"}
+          // setIsVisible={() => setSectionConfig("Section-3")}
+          setVisibility={setVisibilityConfig}
+        />
+      </div>
+    </>
+  );
+};
 
-class About2 extends React.Component {
+export class About2 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -27,7 +87,6 @@ class About2 extends React.Component {
     }
     if (this.state.timerSwitch !== prevState.timerSwitch) {
       if (this.state.timerSwitch) {
-        // Create a new Timer
         this.timer = setInterval(() => {
           console.log("Timer 1");
         }, "1000");
@@ -88,4 +147,3 @@ class About2 extends React.Component {
     );
   }
 }
-export default About2;
