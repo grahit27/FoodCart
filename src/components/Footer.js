87b05003dc9310c099efedb6cb8bb15e2bded116
grahit27 <grahit27@gmail.com>
2023-11-13 18:19:01 +0530

    Updates

--- src/components/Footer.js
+++
@@ -1,6 +0,0 @@
-const Footer = () => {
-  return (
-    <h3 className="p-2 text-center text-blue-700 font-bold">@ 2023 Version</h3>
-  );
-};
-export default Footer;
--- src/utils/UserContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const UserContext = createContext({
-  user: {
-    name: "Tester",
-    email: "tester@123.com",
-  },
-});
-export default UserContext;
